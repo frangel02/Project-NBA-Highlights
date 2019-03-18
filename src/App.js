8534772d2cc32b4c0e8344144ba1413bc79fd64f
frangel(ClaroDom) <frangel02>
2019-03-18 11:03:40 -0400

    route , view and filter de teams

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Home from "./components/home";
+import Teams from "./components/teams";
+import Team from "./components/team";
 
 class App extends Component {
   render() {
@@ -12,7 +14,9 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Header/>
-          <Route exact path="/" component={Home}></Route>          
+          <Route exact path="/" component={Home}></Route>
+           <Route exact path="/teams" component={Teams}></Route>
+          <Route exact path="/team/:id" component={Team}></Route>          
           <Footer/>
         </div>
       </BrowserRouter>
